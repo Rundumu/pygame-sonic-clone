40765c0222d874863562c40b07394becb8077570
Emmanuel <monotone_briggs@hotmail.com>
2021-06-01 23:16:52 +0000

    classified the game, now need to fix performance issues

--- main.py
+++ main.py
@@ -12,17 +12,9 @@ class Game():
 
         self.running = True
         self.clock = pygame.time.Clock()
+        self.camera = Camera(WIDTH, HEIGHT)
     
     def new_game(self):
-        pass
-
-    def run(self):
-        pass
-
-    def draw(self):
-        pass
-
-    def update(self):
         # sprites   
         self.s = Sonic()
         self.ground = Ground(0, HEIGHT - 50, GROUND)
@@ -37,6 +29,25 @@ class Game():
         self.plats = pygame.sprite.Group()
         self.spikes = pygame.sprite.Group()
         self.enemies = pygame.sprite.Group()
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def draw(self):
+        # draw
+        window.fill(BLACK)
+        
+        for sprite in self.sprites:
+            window.blit(sprite.image, self.camera.apply(sprite))
+
+        pygame.display.flip()
+
+    def update(self):
 
         # procedural generation (not sure)
         for ring in RING_LIST:
@@ -64,16 +75,181 @@ class Game():
             self.enemies.add(enemy)
             self.sprites.add(enemy)
 
+        # update
+
+        self.rings.update()
+        self.enemy.update()
+        self.spikes.update()
+        self.sprites.update()
+
+
+        if self.s.vel.y > 0:
+            collisions = pygame.sprite.spritecollide(self.s, self.floor, False)
+
+            if collisions:
+                for collision in collisions:
+                    # TODO: create different classes of platforms each with their own collision properties (done)
+                    if self.s.pos.y > collision.rect.top: 
+                        self.s.pos.y = collision.rect.top
+                        self.s.rect.bottom = self.s.pos.y
+                        self.s.vel.y = 0 
 
 
 
+        hits = pygame.sprite.spritecollide(self.s, self.plats, False)
+
+        if hits:
+            for hit in hits:
+                if abs(self.s.pos.x - hit.rect.left) < 10 and self.s.vel.x > 0:
+                    self.s.pos.x = (hit.rect.left - 1)
+                    self.s.pos.x = self.s.rect.left
+                    self.s.vel.x = 0  
+                if abs(self.s.pos.x - hit.rect.right) < 10 and self.s.vel.x < 0:
+                    self.s.rect.left = (hit.rect.right + 1)
+                    self.s.pos.x = self.s.rect.left
+                    self.s.vel.x = 0
+                if abs(self.s.pos.y - hit.rect.top) < 10 and self.s.vel.y > 0:
+                    self.s.pos.y = hit.rect.top
+                    self.s.rect.bottom = self.s.pos.y
+                    self.s.vel.y = 0
+                if abs(self.s.rect.top - hit.rect.bottom) < 10 and self.s.vel.y < 0:
+                    self.s.rect.top = (hit.rect.bottom + 1) 
+                    self.s.pos.y = self.s.rect.bottom
+                    self.s.vel.y = 0
+            
+        ramp_hits = pygame.sprite.spritecollide(self.s, self.ramps, False)
+
+        if ramp_hits:
+            for hit in ramp_hits:
+                rel_x = self.s.pos.x - hit.rect.x
+
+                if hit.ramp == 1:
+                    pos_height = rel_x + self.s.rect.width
+                    pos_height = min(pos_height, ramp_hits[0].rect.height)
+                    pos_height = max(pos_height, 0)               
+                elif hit.ramp == 2:
+                    pos_height = hit.rect.width - rel_x
+                    pos_height = min(pos_height, ramp_hits[0].rect.height)
+                    pos_height = max(pos_height, 0)
+                if hit.ramp == 2 and abs(self.s.rect.right - hit.rect.left) < 10 and self.s.vel.x > 0:
+                    self.s.rect.right = hit.rect.left
+                    self.s.pos.x = hit.rect.left
+                    self.s.vel.x = 0
+
+                pos_height = min(pos_height, ramp_hits[0].rect.height)
+                pos_height = max(pos_height, 0)
 
 
 
+                target_y = hit.rect.y + hit.rect.height - pos_height
 
+                if self.s.rect.bottom > target_y:
+                    self.s.rect.bottom = target_y
+                    self.s.pos.y = self.s.rect.bottom
+
+        ring_hits = pygame.sprite.spritecollide(self.s, self.rings, False)
+
+        if ring_hits:
+            RING_COUNT += 1
+            print(RING_COUNT)
+            ring_hits[0].kill()
+
+        pricks = pygame.sprite.spritecollide(self.s, self.spikes, False)
+
+        if pricks:
+            for prick in pricks:
+                if self.s.vel.y > 0 and self.s.rect.bottom > prick.rect.top:
+                    if RING_COUNT > 0:
+                        RING_COUNT = RING_COUNT / 2
+                        r = Ring(self.s.pos.x - 180, self.s.rect.centery)
+                        self.rings.add(r)
+                        self.sprites.add(r)
+                        window.blit(r.image, (self.s.pos.x - 180, self.s.rect.centery))
+                    else:
+                        pass
+                    self.s.rect.bottom = prick.rect.top
+                    self.s.pos.y = self.s.rect.bottom
+                    self.s.pos.x = (prick.rect.left - 50)
+                    self.s.vel.y = 0
+                    self.s.vel.x = 0
+                    RING_COUNT = 0
+                    print(RING_COUNT)
+                if self.s.vel.x > 0 and self.s.rect.right > prick.rect.left:
+                    self.s.rect.right = (prick.rect.left - 50)
+                    self.s.pos.x = self.s.rect.right
+                    self.s.vel.x = 0
+                    self.s.vel.y = 0
+                if self.s.vel.x < 0 and self.s.rect.left < prick.rect.right:
+                    self.s.rect.left = (prick.rect.right + 50)
+                    self.s.pos.x = self.s.rect.left
+                    self.s.vel.x = 0
+                    self.s.vel.y = 0
+
+
+
+        # TODO: create an exploding list that generates rings (done)
+
+        clashes = pygame.sprite.spritecollide(self.s, self.enemies, False)
+
+        CURRENT = RING_COUNT
+        if clashes:
+            for clash in clashes:
+                if self.s.vel.y > 0 and abs(clash.rect.top - self.s.rect.bottom) < 5:
+                    self.s.vel.y = 0
+                    clash.vel = 0
+                    self.s.rect.bottom = clash.rect.top
+                    self.s.pos.y = self.s.rect.bottom
+                    clash.kill()
+
+                if abs(self.s.rect.left - clash.rect.right) < 10:
+                    if RING_COUNT > 0:
+                        RING_COUNT = RING_COUNT / 2
+                        r = Ring(self.s.pos.x + 80, self.s.rect.centery)
+                        self.rings.add(r)
+                        self.sprites.add(r)
+                        window.blit(r.image, (self.s.pos.x + 80, self.s.rect.centery))
+                    else:
+                        pass
+                    self.s.rect.left = (clash.rect.right + 50)
+                    self.s.pos.x = self.s.rect.left
+                    clash.vel = (clash.vel * -1)
+                    RING_COUNT = 0 
+                    # TODO: study collisions with moving objects
+                    print(RING_COUNT)
+                
+                if abs(self.s.rect.right - clash.rect.left) < 10:
+                    if RING_COUNT > 0:
+                        RING_COUNT = RING_COUNT / 2
+                        r = Ring(self.s.pos.x - 80, self.s.rect.centery)
+                        self.rings.add(r)
+                        self.sprites.add(r)
+                        window.blit(r.image, (self.s.pos.x - 80, self.s.rect.centery))
+                    else:
+                        pass
+                    
+                    self.s.rect.right = (clash.rect.left - 50)
+                    self.s.pos.x = self.s.rect.right
+                    clash.vel = (clash.vel * -1)
+                    RING_COUNT = 0 
+                    # TODO: study collisions with moving objects
+                    print(RING_COUNT)
+            
+        camera.update(self.s)
 
     def events(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                plat_hits = pygame.sprite.spritecollide(self.s, self.floor, False)
+
+
+                if event.key == pygame.K_SPACE and self.s.jumping == False and self.s.vel.y > -3:
+                    self.s.jump()
+
+            if event.type == pygame.KEYUP: 
+                if event.key == pygame.K_SPACE:
+                    self.s.limit_jump()
 
     def game_over():
         pass
@@ -83,8 +259,6 @@ class Game():
 
 
 
-clock = pygame.time.Clock()
-running = True
 
 current_dir = path.dirname(__file__)
 maps_dir = path.join(current_dir, 'maps')
@@ -98,7 +272,7 @@ maps_dir = path.join(current_dir, 'maps')
 #                                         [x2, y2], [x3, y3]])
 
 # initialise camera
-camera = Camera(WIDTH, HEIGHT)
+
 
 
 
@@ -145,195 +319,7 @@ camera = Camera(WIDTH, HEIGHT)
 # TODO: figure out a way to blit the camera (done)
 
 
-while running:
-    clock.tick(FPS)
+g = Game()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            plat_hits = pygame.sprite.spritecollide(s, floor, False)
-
-
-            if event.key == pygame.K_SPACE and s.jumping == False and s.vel.y > -3:
-                s.jump()
-
-        if event.type == pygame.KEYUP: 
-            if event.key == pygame.K_SPACE:
-                s.limit_jump()
-
-    # update
-
-    rings.update()
-    enemy.update()
-    spikes.update()
-    sprites.update()
-
-
-    if s.vel.y > 0:
-        collisions = pygame.sprite.spritecollide(s, floor, False)
-
-        if collisions:
-            for collision in collisions:
-                # TODO: create different classes of platforms each with their own collision properties (done)
-                if s.pos.y > collision.rect.top: 
-                    s.pos.y = collision.rect.top
-                    s.rect.bottom = s.pos.y
-                    s.vel.y = 0 
-
-
-
-    hits = pygame.sprite.spritecollide(s, plats, False)
-
-    if hits:
-        for hit in hits:
-            if abs(s.pos.x - hit.rect.left) < 10 and s.vel.x > 0:
-                s.pos.x = (hit.rect.left - 1)
-                s.pos.x = s.rect.left
-                s.vel.x = 0  
-            if abs(s.pos.x - hit.rect.right) < 10 and s.vel.x < 0:
-                s.rect.left = (hit.rect.right + 1)
-                s.pos.x = s.rect.left
-                s.vel.x = 0
-            if abs(s.pos.y - hit.rect.top) < 10 and s.vel.y > 0:
-                s.pos.y = hit.rect.top
-                s.rect.bottom = s.pos.y
-                s.vel.y = 0
-            if abs(s.rect.top - hit.rect.bottom) < 10 and s.vel.y < 0:
-                s.rect.top = (hit.rect.bottom + 1) 
-                s.pos.y = s.rect.bottom
-                s.vel.y = 0
-        
-    ramp_hits = pygame.sprite.spritecollide(s, ramps, False)
-
-    if ramp_hits:
-        for hit in ramp_hits:
-            rel_x = s.pos.x - hit.rect.x
-
-            if hit.ramp == 1:
-                pos_height = rel_x + s.rect.width
-                pos_height = min(pos_height, ramp_hits[0].rect.height)
-                pos_height = max(pos_height, 0)               
-            elif hit.ramp == 2:
-                pos_height = hit.rect.width - rel_x
-                pos_height = min(pos_height, ramp_hits[0].rect.height)
-                pos_height = max(pos_height, 0)
-            if hit.ramp == 2 and abs(s.rect.right - hit.rect.left) < 10 and s.vel.x > 0:
-                s.rect.right = hit.rect.left
-                s.pos.x = hit.rect.left
-                s.vel.x = 0
-
-            pos_height = min(pos_height, ramp_hits[0].rect.height)
-            pos_height = max(pos_height, 0)
-
-
-
-            target_y = hit.rect.y + hit.rect.height - pos_height
-
-            if s.rect.bottom > target_y:
-                s.rect.bottom = target_y
-                s.pos.y = s.rect.bottom
-
-    ring_hits = pygame.sprite.spritecollide(s, rings, False)
-
-    if ring_hits:
-        RING_COUNT += 1
-        print(RING_COUNT)
-        ring_hits[0].kill()
-
-    pricks = pygame.sprite.spritecollide(s, spikes, False)
-
-    if pricks:
-        for prick in pricks:
-            if s.vel.y > 0 and s.rect.bottom > prick.rect.top:
-                if RING_COUNT > 0:
-                    RING_COUNT = RING_COUNT / 2
-                    r = Ring(s.pos.x - 180, s.rect.centery)
-                    rings.add(r)
-                    sprites.add(r)
-                    window.blit(r.image, (s.pos.x - 180, s.rect.centery))
-                else:
-                    pass
-                s.rect.bottom = prick.rect.top
-                s.pos.y = s.rect.bottom
-                s.pos.x = (prick.rect.left - 50)
-                s.vel.y = 0
-                s.vel.x = 0
-                RING_COUNT = 0
-                print(RING_COUNT)
-            if s.vel.x > 0 and s.rect.right > prick.rect.left:
-                s.rect.right = (prick.rect.left - 50)
-                s.pos.x = s.rect.right
-                s.vel.x = 0
-                s.vel.y = 0
-            if s.vel.x < 0 and s.rect.left < prick.rect.right:
-                s.rect.left = (prick.rect.right + 50)
-                s.pos.x = s.rect.left
-                s.vel.x = 0
-                s.vel.y = 0
-
-
-
-    # TODO: create an exploding list that generates rings (done)
-    
-    clashes = pygame.sprite.spritecollide(s, enemies, False)
-
-    CURRENT = RING_COUNT
-    if clashes:
-        for clash in clashes:
-            if s.vel.y > 0 and abs(clash.rect.top - s.rect.bottom) < 5:
-                s.vel.y = 0
-                clash.vel = 0
-                s.rect.bottom = clash.rect.top
-                s.pos.y = s.rect.bottom
-                clash.kill()
-
-            if abs(s.rect.left - clash.rect.right) < 10:
-                if RING_COUNT > 0:
-                    RING_COUNT = RING_COUNT / 2
-                    r = Ring(s.pos.x + 80, s.rect.centery)
-                    rings.add(r)
-                    sprites.add(r)
-                    window.blit(r.image, (s.pos.x + 80, s.rect.centery))
-                else:
-                    pass
-                s.rect.left = (clash.rect.right + 50)
-                s.pos.x = s.rect.left
-                clash.vel = (clash.vel * -1)
-                RING_COUNT = 0 
-                # TODO: study collisions with moving objects
-                print(RING_COUNT)
-            
-            if abs(s.rect.right - clash.rect.left) < 10:
-                if RING_COUNT > 0:
-                    RING_COUNT = RING_COUNT / 2
-                    r = Ring(s.pos.x - 80, s.rect.centery)
-                    rings.add(r)
-                    sprites.add(r)
-                    window.blit(r.image, (s.pos.x - 80, s.rect.centery))
-                else:
-                    pass
-                
-                s.rect.right = (clash.rect.left - 50)
-                s.pos.x = s.rect.right
-                clash.vel = (clash.vel * -1)
-                RING_COUNT = 0 
-                # TODO: study collisions with moving objects
-                print(RING_COUNT)
-        
-    camera.update(s)
-
-
-    # draw
-    window.fill(BLACK)
-    
-    for sprite in sprites:
-        window.blit(sprite.image, camera.apply(sprite))
-    
-    
-    
-
-#TODO: Create a way to dynamically generate platforms
-
-    # double buffering 
-    pygame.display.flip()
+while g.running:
+    g.new_game()
